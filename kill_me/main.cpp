b4a95dbaa049db51f891f8a892f4bb7da92fa79a
0xchroma <oli@brokencode.co.uk>
2017-12-15 22:56:34 +0000

    wooo audio

--- kill_me/main.cpp
+++ kill_me/main.cpp
@@ -1,37 +1,22 @@
 
 #include <iostream>
 #include <list>
+#include <chrono>
+#include <thread>
 
-#include <Audioclient.h>
-#include <mmdeviceapi.h>
+#include <portaudio.h>
 
-void init_audio()
+#pragma comment(lib, "C:/libs/portaudio/build/msvc/x64/Debug/portaudio_x64.lib")
+
+#define SAMPLERATE	44100
+#define PI			3.14159265
+#define PI2			6.2831853
+
+struct MIDIEvent
 {
-	IAudioClient* audio_client = nullptr;
-	IMMDevice* device = nullptr;
-	IMMDeviceEnumerator* device_enum = nullptr;
-	WAVEFORMATEX* wave_format;
-	
-	CoInitialize(nullptr);
-
-	CoCreateInstance(
-		CLSID_MMDeviceEnumerator,
-		nullptr,
-		CLSCTX_ALL, IID_IMMDeviceEnumerator,
-		(void**)&device_enum);
-
-	device_enum->GetDefaultAudioEndpoint(EDataFlow::eRender, ERole::eMultimedia, &device);
-	device->Activate(IID_IAudioClient, CLSCTX_ALL, nullptr, (void**)&audio_client);
-	audio_client->GetMixFormat(&wave_format);
-	audio_client->Initialize(AUDCLNT_SHAREMODE_SHARED, 0, 10000000, 0, wave_format, nullptr);
-
-
-}
-
-void shutdown_audio()
-{
-
-}
+	char data[4];
+	unsigned long timestamp;
+};
 
 struct Param
 {
@@ -47,24 +32,177 @@ struct Sample
 
 struct AudioUnit
 {
+	char name[32];
 	std::list<Param> params;
 
-	AudioUnit(std::list<Param> _params) : params(_params) { }
+	AudioUnit(const char* _name) { memcpy(name, _name, strlen(_name)); }
+
 	virtual void process(Sample* input, Sample* output, size_t sample_count) = 0;
+
+protected:
+	void add_param(char* name, float value)
+	{
+		Param p;
+
+		memcpy(p.name, name, strlen(name));
+		p.value = value;
+
+		params.push_back(p);
+	}
+};
+
+enum class OscType { Sine, Saw, Square, Triangle };
+struct Osc
+{
+	OscType type;
+	float phase = 0;
+
+	float step(float amp, float freq)
+	{
+		float v = 0;
+
+		switch (type)
+		{
+		case OscType::Sine:
+		{
+			v = amp * std::sin(phase);
+		} break;
+
+		case OscType::Saw:
+		{
+			v = amp - (amp / PI * phase);
+		} break;
+
+		case OscType::Square:
+		{
+			if (phase < PI)
+				v = amp;
+			else
+				v = -amp;
+		} break;
+
+		case OscType::Triangle:
+		{
+			if (phase < PI)
+				v = -amp + (2 * amp / PI) * phase;
+			else
+				v = amp - (2 * amp / PI) * phase;
+		} break;
+		}
+
+		phase = phase + ((PI2 * freq) / SAMPLERATE);
+
+		if (phase > (PI2))
+			phase = phase - (PI2);
+
+		return v;
+	}
+};
+
+struct BitchFace : public AudioUnit
+{
+	Osc oscs[3];
+
+	BitchFace() : AudioUnit("BitchFace")
+	{
+
+	}
+
+	void process(Sample* input, Sample* output, size_t sample_count) override
+	{
+		for (int i = 0; i < sample_count; i++)
+		{
+			output[i].l = oscs[0].step(0.9f, 440.0f);
+		}
+	}
+};
+
+struct TimeMachine : public AudioUnit
+{
+	TimeMachine() : AudioUnit("TimeMachine")
+	{
+	}
+};
+
+struct Channel
+{
+	AudioUnit* inst = nullptr;
+	std::list<AudioUnit*> effects;
+
+	float vol = 1.0f;
+	float pan = 0.0f;
+
+	void process(Sample* samples, size_t sample_count)
+	{
+		inst->process(nullptr, samples, sample_count);
+
+		for (auto& fx : effects)
+			fx->process(samples, samples, sample_count);
+	}
+};
+
+struct AudioRenderer;
+static int audio_callback(
+	const void* input_buffer,
+	void* output_buffer,
+	unsigned long frame_count,
+	const PaStreamCallbackTimeInfo* time_info,
+	PaStreamCallbackFlags status_flags,
+	void* user_data
+)
+{
+	auto renderer = reinterpret_cast<AudioRenderer*>(user_data);
+	auto sample = reinterpret_cast<Sample*>(output_buffer);
+
+	for (int i = 0; i < frame_count; i++)
+	{
+	}
+
+	return 0;
+}
+
+struct AudioRenderer
+{
+	std::list<Channel> channels;
+
+	PaStream* stream = nullptr;
+
+	AudioRenderer()
+	{
+		Pa_Initialize();
+		Pa_OpenDefaultStream(
+			&stream,
+			0,
+			2,
+			paFloat32,
+			SAMPLERATE,
+			512,
+			audio_callback,
+			reinterpret_cast<void*>(this));
+	}
+
+	~AudioRenderer()
+	{
+		stop();
+		Pa_Terminate();
+	}
+
+	void start()
+	{
+		Pa_StartStream(stream);
+	}
+
+	void stop()
+	{
+		Pa_StopStream(stream);
+	}
 };
 
 int main(int, char**)
 {
-	AudioUnit* generator = nullptr;
-	std::list<AudioUnit*> effects;
-
-	Sample* samples = new Sample[512];
-
-	generator->process(nullptr, samples, 512);
-
-	for (auto& fx : effects)
+	while (true)
 	{
-		fx->process(samples, samples, 512);
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
 	}
 
 	return 0;
